e0e44dfd21028278e3ff969d05d86dfa1ddb87b7
maraungu <86411069+maraungu@users.noreply.github.com>
2021-11-15 13:12:09 +0100

    some refactoring

--- egui-lib/src/draw.rs
+++ egui-lib/src/draw.rs
@@ -3,12 +3,11 @@ use eframe::egui::*;
 use polygon::poly::Poly;
 use std::vec;
 
-// we try here like in painting demo
+// we follow the egui painting demo
 
 pub struct PolyDraw {
     pub points: Vec<Pos2>,
     pub polygon: Poly,
-    // triangles: Vec<Vec<Pos2>>
 }
 
 impl Default for PolyDraw {
@@ -25,45 +24,30 @@ impl PolyDraw {
         let (response, painter) =
             ui.allocate_painter(ui.available_size_before_wrap(), Sense::click());
 
-        // let to_screen = emath::RectTransform::from_to(
-        //     Rect::from_center_size(Pos2::ZERO, response.rect.square_proportions()),
-        //     response.rect,
-        // );
-        // let from_screen = to_screen.inverse();
-
+        // ------ Shape vectors to be added to the painter -------
         let mut points_shapes: Vec<Shape> = vec![];
         let mut lines_shapes: Vec<Shape> = vec![];
         let mut triangles_shapes: Vec<Shape> = vec![];
         let mut convex_shapes: Vec<Shape> = vec![];
+        // --------------------------------------------------------
 
         // poly vertices drawn by clicking on canvas
         if let Some(mut pointer_pos) = response.interact_pointer_pos() {
-            //dbg!(pointer_pos);
             // truncating...otherwise get point repetition due to
             // too high precision
             pointer_pos = Pos2::from([
                 f32::trunc(pointer_pos.x * 10.0) / 10.0,
                 f32::trunc(pointer_pos.y * 10.0) / 10.0,
             ]);
-            //dbg!(pointer_pos);
 
             if !self.points.contains(&pointer_pos) {
                 self.points.push(pointer_pos);
-                //dbg!(&self.points);
-
-                // let canvas_pos = from_screen * pointer_pos;
-                // dbg!(canvas_pos);
-                // let new_canvas_pos = [canvas_pos.x, -canvas_pos.y];
-                // dbg!(new_canvas_pos);
-                // // TODO: truncate this too
-                // self.polygon.vertices.push(new_canvas_pos);
-
                 let transformed_pos: [f32; 2] = [pointer_pos.x, -pointer_pos.y];
                 self.polygon.vertices.push(transformed_pos);
             }
         }
 
-        // poly edges drawn by connecting the vertices in order
+        // poly edges drawn by connecting the points in order
         for (idx, point) in self.points.iter().enumerate() {
             lines_shapes.push(Shape::LineSegment {
                 points: [*point, self.points[(idx + 1) % self.points.len()]],
@@ -83,22 +67,21 @@ impl PolyDraw {
             }));
         }
 
+        // triangles obtained from Delaunay triangulation
         for (idx, triangle) in self.polygon.triangles.iter().enumerate() {
             let points_for_shape: Vec<Pos2> = vec![
                 Pos2::from([triangle[0].x, -triangle[0].y]),
                 Pos2::from([triangle[1].x, -triangle[1].y]),
                 Pos2::from([triangle[2].x, -triangle[2].y]),
             ];
-            //dbg!(&points_for_shape);
-            //dbg!(idx);
 
             let colour_triangles = |colour: Color32| {
                 triangles_shapes.push(Shape::convex_polygon(
                     points_for_shape,
                     colour,
                     Stroke {
-                        width: 1.0,
-                        color: Color32::BLACK,
+                        width: 2.0,
+                        color: Color32::DARK_GRAY,
                     },
                 ));
             };
@@ -109,17 +92,39 @@ impl PolyDraw {
                 2 => colour_triangles(Color32::from_rgba_premultiplied(115, 23, 43, 255)),
                 _ => colour_triangles(Color32::from_rgba_premultiplied(52, 133, 75, 255)),
             }
-
-            // triangles_shapes.push(Shape::convex_polygon(
-            //     points_for_shape,
-            //     Color32::BLUE,
-            //     Stroke::none(),)
-            // );
         }
 
-        painter.extend(points_shapes);
+        // convex part of the polygon
+        for (idx, convex_part) in self.polygon.convex_parts.iter().enumerate() {
+            let mut points_for_shape: Vec<Pos2> = Vec::new();
+            for vertex in convex_part {
+                points_for_shape.push(Pos2::from([vertex.x, -vertex.y]));
+            }
+
+            let colour_parts = |colour: Color32| {
+                convex_shapes.push(Shape::convex_polygon(
+                    points_for_shape,
+                    colour,
+                    Stroke {
+                        width: 2.0,
+                        color: Color32::DARK_GRAY,
+                    },
+                ));
+            };
+
+            match idx % 4 {
+                0 => colour_parts(Color32::from_rgb(99, 164, 186)),
+                1 => colour_parts(Color32::from_rgb(191, 128, 189)),
+                2 => colour_parts(Color32::from_rgb(143, 191, 128)),
+                _ => colour_parts(Color32::from_rgb(235, 233, 117)),
+            }
+        }
+
+        // adding to the painter
         painter.extend(lines_shapes);
         painter.extend(triangles_shapes);
+        painter.extend(convex_shapes);
+        painter.extend(points_shapes);
 
         response
     }
--- polygon/src/poly.rs
+++ polygon/src/poly.rs
@@ -1,11 +1,9 @@
-use std::collections::{HashMap, HashSet};
-
 use cgmath::Point2;
-use nalgebra::Point;
 use smart_default::SmartDefault;
 use spade::delaunay::DelaunayWalkLocate;
 use spade::delaunay::*;
 use spade::kernels::FloatKernel;
+use std::collections::{HashMap, HashSet};
 
 #[derive(SmartDefault)]
 pub struct Poly {
@@ -20,51 +18,54 @@ pub struct Poly {
 }
 
 impl Poly {
+    /// Triangulates the Poly in place.  Uses constrained Delaunay as
+    /// from the spade crate.
+    /// The resulting triangles are stored in the triangles field of Poly
     pub fn triang(&mut self) {
+        // ------ Triangulation -----------
         self.triangulation = FloatCDT::with_walk_locate();
-        //self.triangulation = FloatCDT::with_tree_locate();
 
         for v in self.vertices.iter() {
             self.triangulation.insert(Point2::new(v[0], v[1]));
         }
 
+        // add polygon edges as constraints in the CDT
         for (idx, v) in self.vertices.iter().enumerate() {
             let w = self.vertices[(idx + 1) % self.vertices.len()];
             self.triangulation
                 .add_constraint_edge(Point2::new(v[0], v[1]), Point2::new(w[0], w[1]));
         }
 
+        // Need to collect and remove the "bad edges" of the triangulation
+        // They occur because the triangulation is of the convex hull of
+        // the poly vertices.
         let mut convex_hull: Vec<usize> = vec![];
         let convex_hull_iter = self.triangulation.infinite_face().adjacent_edges();
         for edge in convex_hull_iter {
             let fixed_edge = edge.fix();
-            dbg!(&self.triangulation.edge(fixed_edge));
             convex_hull.push(fixed_edge);
         }
 
-        dbg!(&convex_hull);
-
-        // let mut bad_edges: HashSet<usize> = HashSet::new();
         self.bad_edges = HashSet::new();
 
         // Here we add the most exterior bad edges
+        // They are defined as edges of the convex hull
+        // that are not also polygon edges
         for edge_idx in convex_hull.iter() {
             if !self.triangulation.is_constraint_edge(*edge_idx) {
                 self.bad_edges.insert(*edge_idx);
             }
         }
 
-        dbg!(&self.bad_edges);
-
         let mut to_be_visited = self.bad_edges.clone();
 
+        // loop through the neighbours of the bad edges
+        // if they are outside the poly they are also inserted in the bad_edges vector
         for _ in 0..10 {
             let mut newer_bad_edges: HashSet<usize> = HashSet::new();
 
             for edge in to_be_visited.iter() {
-                //dbg!(edge);
                 for nb in self.triangulation.edge(*edge).o_next_iterator() {
-                    //dbg!(nb.fix());
                     if !self.triangulation.is_constraint_edge(nb.fix())
                         && !self.triangulation.is_constraint_edge(nb.sym().fix())
                     {
@@ -77,26 +78,15 @@ impl Poly {
             to_be_visited = newer_bad_edges;
         }
 
+        // loop through the triangles and check if they contain bad edges
+        // if yes, then ignore; if no, then add to the triangles field
         for face in self.triangulation.triangles() {
             let triangle = face.as_triangle();
             let mut should_add = true;
-            println!(
-                "Found triangle: {:?} -> {:?} -> {:?}",
-                *triangle[0], *triangle[1], *triangle[2]
-            );
 
             for edge in face.adjacent_edges() {
                 let fixed_edge = edge.fix();
-                //dbg!(fixed_edge);
-
-                // order matters! -----
-                let other_edge;
-                if fixed_edge % 2 == 0 {
-                    other_edge = fixed_edge + 1;
-                } else {
-                    other_edge = fixed_edge - 1;
-                }
-                // ---------------------
+                let other_edge = edge.sym().fix();
 
                 if self.bad_edges.contains(&fixed_edge) || self.bad_edges.contains(&other_edge) {
                     should_add = false;
@@ -108,19 +98,14 @@ impl Poly {
                     .push([*triangle[0], *triangle[1], *triangle[2]]);
             }
         }
-
-        for vertex in self.triangulation.vertices() {
-            dbg!(vertex);
-            for e in vertex.ccw_out_edges() {
-                if !self.bad_edges.contains(&e.fix()) {
-                    dbg!(e);
-                }
-            }
-        }
     }
 
+    /// Implementation of the Hertel-Mehlhorn convex decomposition
+    /// of a polygon.  Starts from a triangulation and
+    /// eliminates all triangle edges that are not essential, i.e.
+    /// whose elimination does not make an angle concave.
     pub fn decomposition(&mut self) {
-        // label constraints by 2, 1 = essential, 0 = non-essential, not known =-1
+        // labels: 2 = poly edges, 1 = essential, 0 = non-essential
         let mut edge_labels: HashMap<FixedEdgeHandle, i32> = HashMap::new();
 
         let triangulation_edges = self
@@ -128,12 +113,7 @@ impl Poly {
             .edges()
             .filter(|e| !self.bad_edges.contains(&e.fix()));
 
-        for edge in triangulation_edges
-        // for edge in self
-        //     .triangulation
-        //     .edges()
-        //     .filter(|e| !self.bad_edges.contains(&e.fix()))
-        {
+        for edge in triangulation_edges {
             let fixed_edge = edge.fix();
             let mirror_edge = edge.sym().fix();
 
@@ -157,12 +137,11 @@ impl Poly {
                 .triangulation
                 .vertex(self.previous_vertex(vertex_fixed));
             let vertex_coords = *vertex;
-            
 
             let mut outgoing_edges: Vec<EdgeHandle<Point2<f32>, CdtEdge>> = vertex
-            .ccw_out_edges()
-            .filter(|e| edge_labels.get(&e.fix()).is_some())
-            .collect();
+                .ccw_out_edges()
+                .filter(|e| edge_labels.get(&e.fix()).is_some())
+                .collect();
 
             loop {
                 let mut check_again = false;
@@ -179,18 +158,14 @@ impl Poly {
                     let opp_next = *self.triangulation.vertex(self.next_vertex(opposite_vertex));
                     let opp_coords = *edge.to();
 
-                    let label = edge_labels.get(&fixed_edge).unwrap();
-                    dbg!(label);
+                    let label = edge_labels.get(&fixed_edge).unwrap(); // ok to unwrap becaus of the filter used for outgoing_edges
 
                     // if edge is not known to be essential or poly edge
                     if label == &0 {
-                        dbg!(vertex_fixed);
-                        dbg!(opposite_vertex);
                         // if the vertex and its opposite wrt this diagonal are convex, then not essential
                         if self.convex_angle(vertex_coords, prev_poly_vertex, next_poly_vertex)
                             && self.convex_angle(opp_coords, opp_prev, opp_next)
                         {
-                            dbg!("convex");
                             outgoing_edges.remove(idx);
                             check_again = true;
                             break;
@@ -203,30 +178,17 @@ impl Poly {
                             [(idx + outgoing_edges.len() - 1) % outgoing_edges.len()]
                         .to();
                         let next_vert = *outgoing_edges[(idx + 1) % outgoing_edges.len()].to();
-                        let previous = outgoing_edges
-                        [(idx + outgoing_edges.len() - 1) % outgoing_edges.len()]
-                    .to().fix();
-                    let next = outgoing_edges[(idx + 1) % outgoing_edges.len()].to().fix();
-                        dbg!(previous);
-                        dbg!(next);
 
+                        // note the order switch!
                         if !self.convex_angle(vertex_coords, next_vert, prev_vert) {
-
-                            dbg!("essential");
                             *edge_labels.get_mut(&fixed_edge).unwrap() = 1;
                             *edge_labels.get_mut(&mirror_edge).unwrap() = 1;
                             continue;
-                        }
-                        else //if self.convex_angle(opp_coords, opp_prev, opp_next) 
-                        {
-                            dbg!("can remove");
+                        } else {
                             outgoing_edges.remove(idx);
                             check_again = true;
                             break;
                         }
-
-                        
-                        
                     }
                 }
 
@@ -235,15 +197,9 @@ impl Poly {
                 }
             }
         }
-        dbg!("checking");
-        for edge in self.triangulation.edges() {
-            if let Some(value) = edge_labels.get(&edge.fix()) {
-                if value == &1 {
-                    dbg!(edge);
-                }
-            }
-        }
+
         // -------- Gluing together the triangles along the non-essential edges ----------
+        // First add all triangles to the convex_polys vector
         let mut convex_polys: Vec<Vec<EdgeHandle<Point2<f32>, CdtEdge>>> = Vec::new();
         for triang in self.triangulation.triangles() {
             let mut triangle: Vec<EdgeHandle<Point2<f32>, CdtEdge>> = Vec::new();
@@ -251,31 +207,30 @@ impl Poly {
             for edge in triang.adjacent_edges() {
                 if !self.bad_edges.contains(&edge.fix()) {
                     triangle.push(edge);
-                }
-                else {
+                } else {
                     do_not_add = true;
                 }
             }
-            if !do_not_add {convex_polys.push(triangle);}
+            if !do_not_add {
+                convex_polys.push(triangle);
+            }
         }
 
         for (edge, value) in edge_labels.iter() {
+            // find the polygons containing this non-essential edge
+            // collect their unique vertices minus the non-essential
+            // edge in a new_poly
             if value == &0 {
                 let edge_handle = self.triangulation.edge(*edge);
-                dbg!(edge_handle);
                 let mut new_poly: Vec<EdgeHandle<Point2<f32>, CdtEdge>> = Vec::new();
-                
 
-                //dbg!(&convex_polys);
                 for poly in convex_polys.iter() {
-                    if poly.contains(&edge_handle)   {
+                    if poly.contains(&edge_handle) {
                         for e in poly.iter().filter(|e| **e != edge_handle) {
                             if !new_poly.contains(e) && !new_poly.contains(&e.sym()) {
                                 new_poly.push(*e);
                             }
                         }
-                        
-                        
                     }
                     if poly.contains(&edge_handle.sym()) {
                         for e in poly.iter().filter(|e| **e != edge_handle) {
@@ -283,29 +238,33 @@ impl Poly {
                                 new_poly.push(*e);
                             }
                         }
-                        
                     }
                 }
 
-                convex_polys = convex_polys.into_iter().filter(|poly| !poly.contains(&edge_handle) &&
-            !poly.contains(&edge_handle.sym())).collect();
+                convex_polys = convex_polys
+                    .into_iter()
+                    .filter(|poly| {
+                        !poly.contains(&edge_handle) && !poly.contains(&edge_handle.sym())
+                    })
+                    .collect();
 
                 convex_polys.push(new_poly);
             }
         }
-        dbg!("check convex parts");
+
         for convex_part in convex_polys.iter() {
-            dbg!(convex_part);
             let new_convex_part = self.vertex_ordering(convex_part);
-            dbg!(&new_convex_part);
             self.convex_parts.push(new_convex_part);
         }
-
-
     }
 
-    fn vertex_ordering(&self, convex_poly: &Vec<EdgeHandle<Point2<f32>, CdtEdge>>) 
-    -> Vec<Point2<f32>> {
+    /// Ordering function that ensures that the vertices of the
+    /// convex parts are ordered as follows:
+    /// [point1, point2], [point2, point3], ...
+    fn vertex_ordering(
+        &self,
+        convex_poly: &Vec<EdgeHandle<Point2<f32>, CdtEdge>>,
+    ) -> Vec<Point2<f32>> {
         let mut ordered_poly: Vec<[usize; 2]> = Vec::new();
         let mut final_poly: Vec<Point2<f32>> = Vec::new();
         // first order by vertex index
@@ -319,12 +278,10 @@ impl Poly {
             final_poly.push(*self.triangulation.vertex(vertex[0]));
             final_poly.push(*self.triangulation.vertex(vertex[1]));
         }
-
-        // dbg!(&ordered_poly);
         final_poly
-        //ordered_poly
     }
 
+    /// Returns true if angle is convex
     fn convex_angle(
         &self,
         vertex: Point2<f32>,
